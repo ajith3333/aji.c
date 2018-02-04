33e2f5e14fe38841012aa0f6168d11bf8a3cf7ca
ajith3333 <35687948+ajith3333@users.noreply.github.com>
2018-02-04 14:23:32 +0530

    mu.c

---
+++ mu.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+#include <conio.h>
+void main()
+{
+int a,b;
+printf("\n the values of a ");
+scanf("%d",&a);
+printf("\n the values of b");
+scanf("%d",b);
+if(a*b)
+getch();
+}
